55792711acd0991c919c1d7e5d6fb18769f5e68e
Michael N. Rubinstein <michael@polyblog.com>
2016-08-29 17:03:48 -0700

    cleaned up and made cosmetic change

--- scripts/slackbot.js
+++ scripts/slackbot.js
@@ -4,18 +4,14 @@ var squirrels;
 squirrels = ["http://img.skitch.com/20100714-d6q52xajfh4cimxr3888yb77ru.jpg", "https://img.skitch.com/20111026-r2wsngtu4jftwxmsytdke6arwd.png", "http://cl.ly/1i0s1r3t2s2G3P1N3t3M/Screen_Shot_2011-10-27_at_9.36.45_AM.png", "http://shipitsquirrel.github.com/images/squirrel.png"];
 
 module.exports = function(robot) {
-  robot.hear(/javascript/i, function(msg) {
-    return msg.send("I love JavaScript!");
+  // Basic example of respond / send.
+  // If the user enters hi or hello the bot responds "Greetings, presumed Human."
+  // (Note: .hear does the same thing as .respond, apparently).
+  robot.respond(/hi|hello/i, function(res) {
+    return res.send("Greetings, presumed Human.");
   });
-  robot.hear(/Hello!/i, function(res) {
-    return res.send("Hi there!");
-  });
-  /* Basic example of respond / send. If the user enters hi or hello the bot responds "Howdy!" */
-  robot.respond(/hi|hello/i, function(msg) {
-    return msg.send("Howdy!");
-  });
-  robot.hear(/ship it/i, function(msg) {
-    return msg.send(msg.random(squirrels));
+  robot.respond(/ship it/i, function(res) {
+    return res.send(res.random(squirrels));
   });
   robot.respond(/open the (.*) doors/i, function(res) {
     var doortype = res.match[1];
@@ -27,12 +23,3 @@ module.exports = function(robot) {
   });
 }
 
-  // # robot.hear /badger/i, (res) ->
-  // #   res.send "Badgers? BADGERS? WE DON'T NEED NO STINKIN BADGERS"
-  // #
-  // # robot.respond /open the (.*) doors/i, (res) ->
-  // #   doorType = res.match[1]
-  // #   if doorType is "pod bay"
-  // #     res.reply "I'm afraid I can't let you do that."
-  // #   else
-  // #     res.reply "Opening #{doorType} doors"
